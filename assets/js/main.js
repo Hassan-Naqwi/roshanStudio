8dbf76d3e2037cc44009a7c9184ed689b7ac2860
Hassan-Naqwi <hasannaqwi@gmail.com>
2025-07-26 22:26:59 +0500

    lastm

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,72 +1,37 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     if (window.matchMedia("(min-width: 767px)").matches) {
-//         window.addEventListener('scroll', function () {
-//             let scroll = window.scrollY;
-//             let offset = document.querySelector('#nav').offsetTop;
-//             if (scroll > offset) {
-//                 document.querySelector('#nav').classList.add('sticky');
-//                 document.querySelectorAll('.none').forEach((e) => {
-//                     e.style.display = 'block';
-//                 });
-//             } else {
-//                 document.querySelectorAll('.none').forEach((e) => {
-//                     e.style.display = 'none';
-//                 });
-//                 document.querySelector('.nav').classList.remove('sticky');
-//             }
-//         });
-//     }
-//     $(function () {
-//         $('.toggle').click(function () {
-//             $(this).parent().parent().next().find('ul').slideToggle();
-//         });
-//     });
-// });
 
-function handleMobileMenu() {
-    $('.toggle').click(function () {
-        $(this).parent().parent().next().find('ul').slideToggle();
-    });
-}
-
-function handleScrollSticky() {
-    window.addEventListener('scroll', function () {
-        let scroll = window.scrollY;
-        let offset = document.querySelector('#nav').offsetTop;
-        if (scroll > offset) {
-            document.querySelector('#nav').classList.add('sticky');
-            document.querySelectorAll('.none').forEach(e => {
-                e.style.display = 'block';
-            });
-        } else {
-            document.querySelectorAll('.none').forEach(e => {
-                e.style.display = 'none';
-            });
-            document.querySelector('#nav').classList.remove('sticky');
-        }
-    });
-}
-
-function initLogic() {
-    const isDesktop = window.matchMedia('(min-width: 767px)').matches;
-    if (isDesktop) {
-        handleScrollSticky();
-    } else {
-        handleMobileMenu();
-    }
-}
 
 window.addEventListener('load', function () {
     requestAnimationFrame(() => {
         requestAnimationFrame(() => {
-            initLogic();
+            if (window.matchMedia('(min-width: 767px)').matches) {
+                window.addEventListener('scroll', function () {
+                    let scroll = window.scrollY;
+                    let offset = document.querySelector('#nav').offsetTop;
+                    if (scroll > offset) {
+                        document.querySelector('#nav').classList.add('sticky');
+                        document.querySelectorAll('.none').forEach((e) => {
+                            e.style.display = 'block';
+                        });
+                    } else {
+                        document.querySelectorAll('.none').forEach((e) => {
+                            e.style.display = 'none';
+                        });
+                        document.querySelector('.nav').classList.remove('sticky');
+                    }
+                });
+            } else {
+                $(function () {
+                    $('.toggle').click(function () {
+                        $(this).parent().parent().next().find('ul').slideToggle();
+                    });
+                });
+            }
         });
     });
 });
 
-window.addEventListener('resize', function () {
-    initLogic();
-});
+
+
 
 
 
